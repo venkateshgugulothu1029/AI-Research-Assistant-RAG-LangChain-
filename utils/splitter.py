7ae2a44c49823af0a4b05f752e9436841cdab4c3
venkateshgugulothu1029 <venkateshgugulothu1029@gmail.com>
2026-04-06 09:29:25 +0530

    splitter.py

---
+++ utils/splitter.py
@@ -0,0 +1,8 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def split_documents(documents):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+    return splitter.split_documents(documents)
